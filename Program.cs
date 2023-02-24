c88b4437c4fdd7db7c4095c8b736da47bf8d77a2
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-24 12:49:54 +0530

    Program Stock Details

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Welcome to the Stock Account Management");
             string path = "C:\\Users\\Rahul\\source\\repos\\StockAccountManagement\\StockAccountManagement\\Stock.json";
 
+
             FetchStock fetchStock = new FetchStock();
 
             StockDetails data = fetchStock.Read(path);
@@ -15,12 +16,9 @@
 
             for (int i = 0; i < data.stocks.Count; i++)
             {
-                Console.WriteLine("\n" + data.stocks[i].StockName);
+                Console.WriteLine(data.stocks[i].StockName);
                 Console.WriteLine("shares: " + data.stocks[i].Shares);
                 Console.WriteLine("price: " + data.stocks[i].Price);
-
-                int TotalValue = data.stocks[i].Shares * data.stocks[i].Price;
-                Console.WriteLine("\nThe Total Value of {0} for {1} shares is: {2} ", data.stocks[i].StockName, data.stocks[i].Shares, TotalValue);
                 Console.WriteLine("-------------------------------");
             }
         }
