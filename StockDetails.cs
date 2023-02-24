f505210e0348cb3630112fd4b06578e01a440aef
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-24 12:35:51 +0530

    Program For Stock Value Calculation

---
+++ StockDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    public class Stocks
+    {
+        public string StockName;
+        public int Shares;
+        public int Price;
+    }
+    public class StockDetails
+    {
+        public List<Stocks> stocks;
+    }
+}
